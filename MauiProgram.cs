f5cef21dd4c2a0e4158fecbeea6492ed49dbcedc
migcespedes86-create <migcespedes86@gmail.com>
2025-09-19 17:16:28 -0600

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -8,23 +8,8 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
-
-#if ANDROID
-        Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("AndroidSettings", (handler, view) =>
-        {
-            var ctrl = handler.PlatformView;
-            ctrl.Settings.DomStorageEnabled = true;
-            ctrl.Settings.JavaScriptEnabled = true;
-        });
-#endif
-
+        builder.UseMauiApp<App>();
         return builder.Build();
     }
 }
+
